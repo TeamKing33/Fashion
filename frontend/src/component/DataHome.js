fc676799167540dcc91a28e4e41f6c325c25f0d6
mohamed yasser <teamkingmariout@gmail.com>
2024-01-01 03:05:18 +0200

    update home

---
+++ frontend/src/component/DataHome.js
@@ -0,0 +1,30 @@
+
+
+const DataHome=[
+    {
+        cardHover:"Fashion wave Classic!",
+        cardfigure:"Classic",
+        cardocupation:"We offer the timeless T-shirt classic for sale, embodying comfort and style. Elevate your wardrobe with our premium selection.",
+
+    },
+    {
+        cardHover:"Fashion wave Classic!",
+        cardfigure:"Classic",
+        cardocupation:"We offer the timeless T-shirt classic for sale, embodying comfort and style. Elevate your wardrobe with our premium selection.",
+        
+    },
+    {
+        cardHover:"Fashion wave Classic!",
+        cardfigure:"Classic",
+        cardocupation:"We offer the timeless T-shirt classic for sale, embodying comfort and style. Elevate your wardrobe with our premium selection.",
+        
+    },
+    {
+        cardHover:"Fashion wave Classic!",
+        cardfigure:"Classic",
+        cardocupation:"We offer the timeless T-shirt classic for sale, embodying comfort and style. Elevate your wardrobe with our premium selection.",
+        
+    }
+]
+
+export default DataHome;
