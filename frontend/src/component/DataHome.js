fd93edd0c64ef1be158b0bfef929a074df72a31a
mohamed yasser <teamkingmariout@gmail.com>
2024-01-01 03:40:46 +0200

    update home4

--- frontend/src/component/DataHome.js
+++ frontend/src/component/DataHome.js
@@ -8,21 +8,21 @@ const DataHome=[
 
     },
     {
-        cardHover:"Fashion wave Classic!",
-        cardfigure:"Classic",
-        cardocupation:"We offer the timeless T-shirt classic for sale, embodying comfort and style. Elevate your wardrobe with our premium selection.",
+        cardHover:"Fashion wave Polo!",
+        cardfigure:"Polo",
+        cardocupation:"A Polo Neck T-shirt,  features a distinctive folded collar and a short buttoned placket. Originally associated with sports, it has evolved into a versatile and timeless garment, blending casual comfort with a polished aesthetic.",
         
     },
     {
-        cardHover:"Fashion wave Classic!",
-        cardfigure:"Classic",
-        cardocupation:"We offer the timeless T-shirt classic for sale, embodying comfort and style. Elevate your wardrobe with our premium selection.",
+        cardHover:"Fashion wave  Classic V Neck!",
+        cardfigure:"Classic V Neck",
+        cardocupation:"The Classic V Neck T-shirt is a wardrobe essential, offering a timeless blend of comfort and style. With its simple yet elegant design and versatile appeal, it effortlessly elevates both casual and more polished looks.",
         
     },
     {
-        cardHover:"Fashion wave Classic!",
-        cardfigure:"Classic",
-        cardocupation:"We offer the timeless T-shirt classic for sale, embodying comfort and style. Elevate your wardrobe with our premium selection.",
+        cardHover:"Fashion wave Crew neck!",
+        cardfigure:"Crew neck",
+        cardocupation:"The crew neck T-shirt is a timeless and versatile wardrobe essential, known for its classic rounded neckline and comfortable fit. Its simplicity makes it suitable for various occasions, and it can be easily styled for both casual and more polished looks.",
         
     }
 ]
