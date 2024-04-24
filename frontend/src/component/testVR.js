a02a6e10183a6073d38b49982d7958233304bfb3
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-04-25 00:14:48 +0200

    update project 66

--- frontend/src/component/testVR.js
+++ frontend/src/component/testVR.js
@@ -1,12 +1,94 @@
-import React from 'react'
+import React, { Component } from 'react';
+import 'aframe';
+import mp from './1.mp4'
+import img1 from './1.png'
 
-function testVR() {
-  return (
-    <div style={{height:"100vh"}}>
-      <iframe src="https://data.sentiovr.com/spaces/58711/space_1713984728/vtour/tour.html" height="100%" width="100%"  allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-    </div>
-  )
+class TestVR extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVideo: false
+    };
+    this.handleVideoDisplay = this.handleVideoDisplay.bind(this);
+  }
+
+  handleVideoDisplay() {
+    setTimeout(() => {
+      this.setState({ showVideo: true });
+    }, 3000);
+  }
+
+  componentDidMount() {
+    // يمكنك هنا إضافة أكواد تفاعلية خاصة بتحريك المحتوى بحركة الجهاز
+    // مثال: تحريك كاميرا العالم مع حركة الجهاز
+    const scene = document.querySelector('a-scene');
+    const camera = scene.querySelector('[camera]');
+    
+    window.addEventListener('deviceorientation', event => {
+      const { alpha, beta, gamma } = event;
+      camera.setAttribute('rotation', {
+        x: beta,
+        y: alpha,
+        z: gamma
+      });
+    });
+
+    this.handleVideoDisplay();
+  }
+
+  render() {
+    return (
+      <a-scene>
+        <a-entity camera></a-entity>
+        <a-assets>
+          {/* إضافة أي موارد إضافية هنا (مثل الصور والفيديوهات) */}
+          <img id="myPhoto" src="رابط_صورتك" alt="صورتك"></img>
+        </a-assets>
+        {/* منزل */}
+        <a-entity
+          id="house"
+          gltf-model="url(رابط_ملف_المنزل)"
+          position="0 0 -5"
+        ></a-entity>
+        {/* شاشة التلفاز */}
+        <a-plane
+          id="tv-screen"
+          position="0 2.5 -3.5"
+          rotation="0 0 0"
+          width="2.4"
+          height="1.8"
+          color="#000"
+          onMouseEnter={() => this.setState({ showVideo: true })}
+          onMouseLeave={() => this.setState({ showVideo: false })}
+        >
+          {this.state.showVideo && (
+            <a-video
+              src={mp}
+              width="2.3"
+              height="1.7"
+              position="0 0 0.01"
+            ></a-video>
+          )}
+        </a-plane>
+        {/* إطار الصورة */}
+        <a-plane
+          id="photo-frame"
+          position="-2.5 2.5 -3.5"
+          rotation="0 90 0"
+          width="1.6"
+          height="1.2"
+          color="#fff"
+        >
+          <a-image
+            src={img1}
+            width="1.5"
+            height="1.1"
+            position="0 0 0.01"
+          ></a-image>
+        </a-plane>
+      </a-scene>
+    );
+  }
 }
 
-
-export default testVR
+export default TestVR;
