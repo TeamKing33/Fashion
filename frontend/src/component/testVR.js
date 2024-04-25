52d910dd8a0134669b6f486fdf982483000e41b5
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-04-25 02:43:37 +0200

    update project 67

--- frontend/src/component/testVR.js
+++ frontend/src/component/testVR.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'aframe';
-import mp from './1.mp4'
-import img1 from './1.png'
+import mp from './1.mp4';
+import img1 from './1.png';
 
 class TestVR extends Component {
   constructor(props) {
@@ -19,8 +19,6 @@ class TestVR extends Component {
   }
 
   componentDidMount() {
-    // يمكنك هنا إضافة أكواد تفاعلية خاصة بتحريك المحتوى بحركة الجهاز
-    // مثال: تحريك كاميرا العالم مع حركة الجهاز
     const scene = document.querySelector('a-scene');
     const camera = scene.querySelector('[camera]');
     
@@ -41,19 +39,25 @@ class TestVR extends Component {
       <a-scene>
         <a-entity camera></a-entity>
         <a-assets>
-          {/* إضافة أي موارد إضافية هنا (مثل الصور والفيديوهات) */}
           <img id="myPhoto" src="رابط_صورتك" alt="صورتك"></img>
         </a-assets>
-        {/* منزل */}
+        {/* House */}
         <a-entity
           id="house"
           gltf-model="url(رابط_ملف_المنزل)"
           position="0 0 -5"
         ></a-entity>
-        {/* شاشة التلفاز */}
+        {/* Draw the walls */}
+        <a-box position="0 1.6 -5" width="10" height="3.2" depth="0.1" color="#CCCCCC"></a-box>
+        <a-box position="0 1.6 3" width="10" height="3.2" depth="0.1" color="#CCCCCC"></a-box>
+        <a-box position="-5 1.6 -2" width="0.1" height="3.2" depth="8" color="#CCCCCC"></a-box>
+        <a-box position="5 1.6 -2" width="0.1" height="3.2" depth="8" color="#CCCCCC"></a-box>
+        {/* Floor */}
+        <a-plane position="0 0 -2" rotation="-90 0 0" width="10" height="10" color="#777"></a-plane>
+        {/* TV Screen */}
         <a-plane
           id="tv-screen"
-          position="0 2.5 -3.5"
+          position="0 1.5 -3.9"
           rotation="0 0 0"
           width="2.4"
           height="1.8"
@@ -65,25 +69,25 @@ class TestVR extends Component {
             <a-video
               src={mp}
               width="2.3"
-              height="1.7"
-              position="0 0 0.01"
+              height="1.4"
+              position="0 0 1.4"
             ></a-video>
           )}
         </a-plane>
-        {/* إطار الصورة */}
+        {/* Image Frame */}
         <a-plane
           id="photo-frame"
-          position="-2.5 2.5 -3.5"
+          position="-4.5 1.5 -3.5"
           rotation="0 90 0"
           width="1.6"
           height="1.2"
-          color="#fff"
+          color="#CCCCCC"
         >
           <a-image
             src={img1}
             width="1.5"
             height="1.1"
-            position="0 0 0.01"
+            position="-3 0 0.01"
           ></a-image>
         </a-plane>
       </a-scene>
